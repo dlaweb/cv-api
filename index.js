92001e878b3d9e1f0dbb8e278ef967543f0d5780
David <davidlabbe29@gmail.com>
2019-04-06 14:21:27 +0200

    Create route for see cv

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+import db from  './db/db';
+
+const bp = require('body-parser');
 const express = require("express");
 var app = express()
 
-app.get('/', (req,res) => {
-    res.setHeader('Content-type', 'text/html')
-    res.send("Initialisation de l'application")
 
+app.use(bp.urlencoded({extended:true}))
+
+app.get('/api/v1/cv', (req,res) => {
+    res.status(200).send({
+        success: 'true',
+        message: 'Cv retrieved successfully',
+        cv: db
+    })
 });
 
-app.listen(5000)
-console.log("Running server")
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+});
